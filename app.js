beda11064e6c688e528ff20b333eea56a464faf4
rocky_manobi <tak.koroki@gmail.com>
2018-11-08 19:36:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.send({ message: "ok"});
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
